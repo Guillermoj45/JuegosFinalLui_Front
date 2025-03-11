fe30e8e2150ea4e3dc7969a1a9ab68f23306ab1f
Guillermo <guillermoremesa@gmail.com>
2025-03-11 09:42:06 +0100

    inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
     path: 'juego',
